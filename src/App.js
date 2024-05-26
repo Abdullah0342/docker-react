0774d79d7d935775cf84e9116bf29e0e9c239fa4
abdullah <abdullah@gmail.com>
2024-05-26 12:36:35 +0500

    from feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and OF COURSE save to reload.
+          Edit <code>src/App.js</code> and OF COURSE ,this is from feature branch,save to reload.
         </p>
         <a
           className="App-link"
